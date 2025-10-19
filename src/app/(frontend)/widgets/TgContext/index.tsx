e2c38acc812864d8893c638fd1e3fb24031c3f7c
Gregori <orlov.gregori@yandex.ru>
2025-10-19 17:17:31 +0300

    перенес реализацию получения данных из ТГклиента в контекст

--- src/app/(frontend)/widgets/TgContext/index.tsx
+++ src/app/(frontend)/widgets/TgContext/index.tsx
@@ -1,6 +1,11 @@
 'use client'
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useContext, useState, useEffect, useRef, useCallback } from 'react'
 import { WebApp } from '@twa-dev/types';
+import { validateTgSignature } from '@front/widgets/tgClient/verify-telegram-data'
+import serverLog from '@/utilities/serverLog'
+import ym from 'react-yandex-metrika'
+import { parseStartParams } from '@front/widgets/tgClient'
+import { useUser } from '@front/widgets/UserContext'
 
 interface TgContextProps {
   tg: WebApp | null;
@@ -15,9 +20,99 @@ interface TgProviderProps {
   children: React.ReactNode;
 }
 
+const waitForTelegram = (): Promise<void> => {
+  return new Promise<void>((resolve) => {
+
+    const checkTelegram = () => {
+      if (window.Telegram && window.Telegram.WebApp) {
+        resolve()
+      } else {
+        setTimeout(checkTelegram, 500) // Проверяем каждые 500 миллисекунд
+      }
+    }
+    checkTelegram()
+  })
+}
+
 export const TgContextProvider = ({children}: TgProviderProps) => {
   const [tg, setTg] = useState<WebApp | null>(null);
   const [ isTgReady, setIsTgReady] = useState<boolean>(false);
+  const telegramInitialized = useRef(false);
+  const { user,  setUser } = useUser()
+
+  const checkSignature = useCallback(async (initData: string) => {
+    try {
+      return await validateTgSignature(initData)
+    } catch (err) {
+      throw new Error('Ошибка проверки подписи')
+    }
+  }, [])
+
+
+  useEffect(() => {
+    const initializeTg = async () => {
+      if (telegramInitialized.current) { // Проверка, была ли функция уже вызвана
+        console.log("Telegram already initialized, skipping.");
+        return;
+      }
+      try {
+        await waitForTelegram()
+        const tgInstance = window.Telegram?.WebApp as WebApp
+        if (tgInstance) {
+          setTg(tgInstance)
+          setIsTgReady(true);
+          const initDataUnsafe = tgInstance.initDataUnsafe || {}
+          const user = initDataUnsafe.user
+          if (user) {
+            const processUserData = async () => {
+              const isValid = await checkSignature(tgInstance.initData)
+              if (isValid) {
+                setUser({
+                  id: user.id,
+                  firstName: user.first_name,
+                  lastName: user.last_name,
+                  username: user.username,
+                  photoUrl: user.photo_url,
+                  isDataValid: true,
+                  startParam: initDataUnsafe.start_param
+                })
+                await serverLog('User установлен в контекст.')
+              } else {
+                console.error('Telegram data is not valid!')
+                setUser(prevState => ({...prevState, isDataValid: false}))
+              }
+            }
+            processUserData()
+          } else {
+            console.log('User data not available.')
+            await serverLog('User data not available.')
+          }
+        } else {
+          await serverLog('ТГ не найден')
+        }
+
+
+
+
+      } catch (error) {
+        console.error('Ошибка при инициализации Telegram Web App:', error)
+      } finally {
+        telegramInitialized.current = true;
+      }
+    }
+    initializeTg()
+  }, [])
+
+
+
+
+
+
+
+
+
+
+
 
   return (
     <TgContext.Provider value={{tg, isTgReady}}>
--- src/app/(frontend)/widgets/tgClient/index.tsx
+++ src/app/(frontend)/widgets/tgClient/index.tsx
@@ -75,69 +75,71 @@ export default function TgClient( { children, baseUrl }: ITgClientProps ) {
     }
   }, [])
 
-  useEffect(() => {
-    const fetchData = async () => {
-      if (telegramInitialized.current) { // Проверка, была ли функция уже вызвана
-        console.log("Telegram already initialized, skipping.");
-        return;
-      }
-      try {
-        await waitForTelegram()
-        const tg = window.Telegram?.WebApp as WebApp
-        if (tg) {
-          setDarkMode(tg.colorScheme === 'dark');
-          tg.onEvent('themeChanged', () => {
-            setDarkMode(window.Telegram.WebApp.colorScheme === 'dark');
-          });
-          const initDataUnsafe = tg.initDataUnsafe || {}
-          const user = initDataUnsafe.user
-          if (user) {
-            const processUserData = async () => {
-              const isValid = await checkSignature(tg.initData)
-              if (isValid) {
-                setUser({
-                  id: user.id,
-                  firstName: user.first_name,
-                  lastName: user.last_name,
-                  username: user.username,
-                  photoUrl: user.photo_url,
-                  isDataValid: true,
-                  startParam: initDataUnsafe.start_param
-                })
-                if (initDataUnsafe.start_param) {
-                  const params = parseStartParams(initDataUnsafe.start_param)
-                  if (params) {
-                    const newUrl = buildUrl(params)
-                    const urlForYM = baseUrl ? `${baseUrl}${newUrl}` : newUrl
-                    ym('hit', urlForYM)
-                    router.push(newUrl)
-                  }
-                }
-                await serverLog('User установлен в контекст.')
-              } else {
-                console.error('Telegram data is not valid!')
-                setUser(prevState => ({...prevState, isDataValid: false}))
-              }
-            }
-            processUserData()
-          } else {
-            console.log('User data not available.')
-            await serverLog('User data not available.')
-          }
-        } else {
-          await serverLog('ТГ не найден')
-        }
-      } catch (error) {
-        console.error('Ошибка при инициализации Telegram Web App:', error)
-      } finally {
-        telegramInitialized.current = true;
-      }
-    }
-    fetchData()
-  }, [user])
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     if (telegramInitialized.current) { // Проверка, была ли функция уже вызвана
+  //       console.log("Telegram already initialized, skipping.");
+  //       return;
+  //     }
+  //     try {
+  //       await waitForTelegram()
+  //       const tg = window.Telegram?.WebApp as WebApp
+  //       if (tg) {
+  //         setDarkMode(tg.colorScheme === 'dark');
+  //         tg.onEvent('themeChanged', () => {
+  //           setDarkMode(window.Telegram.WebApp.colorScheme === 'dark');
+  //         });
+  //         const initDataUnsafe = tg.initDataUnsafe || {}
+  //         const user = initDataUnsafe.user
+  //         if (user) {
+  //           const processUserData = async () => {
+  //             const isValid = await checkSignature(tg.initData)
+  //             if (isValid) {
+  //               setUser({
+  //                 id: user.id,
+  //                 firstName: user.first_name,
+  //                 lastName: user.last_name,
+  //                 username: user.username,
+  //                 photoUrl: user.photo_url,
+  //                 isDataValid: true,
+  //                 startParam: initDataUnsafe.start_param
+  //               })
+  //               if (initDataUnsafe.start_param) {
+  //                 const params = parseStartParams(initDataUnsafe.start_param)
+  //                 if (params) {
+  //                   const newUrl = buildUrl(params)
+  //                   const urlForYM = baseUrl ? `${baseUrl}${newUrl}` : newUrl
+  //                   ym('hit', urlForYM)
+  //                   router.push(newUrl)
+  //                 }
+  //               }
+  //               await serverLog('User установлен в контекст.')
+  //             } else {
+  //               console.error('Telegram data is not valid!')
+  //               setUser(prevState => ({...prevState, isDataValid: false}))
+  //             }
+  //           }
+  //           processUserData()
+  //         } else {
+  //           console.log('User data not available.')
+  //           await serverLog('User data not available.')
+  //         }
+  //       } else {
+  //         await serverLog('ТГ не найден')
+  //       }
+  //     } catch (error) {
+  //       console.error('Ошибка при инициализации Telegram Web App:', error)
+  //     } finally {
+  //       telegramInitialized.current = true;
+  //     }
+  //   }
+  //   fetchData()
+  // }, [user])
 
   return (
     <div  style={{ backgroundColor: isDarkMode ? '#222' : '#fff', color: isDarkMode ? '#fff' : '#000' }}>
+      <div>User: {user?.username}</div>
+      <div>User: {startParams?.startParam}</div>
       {children}
     </div>
   )
