f2f94cdb1898f39fd00353d2121ccf55178952ee
Gregori <orlov.gregori@yandex.ru>
2025-12-03 13:29:26 +0300

    Переписал страницу с формой

--- src/app/(frontend)/event/page.tsx
+++ src/app/(frontend)/event/page.tsx
@@ -10,18 +10,24 @@ interface IPageProps {
 export default function TrialFormPage ({params: paramsPromise}: IPageProps) {
   return (
     <div className={styles.bg}>
-      <h1 className={styles.title}>Записаться на Helloween-вечеринку</h1>
+      <h1 className={styles.title}>Записаться вечеринку
+        <span className={styles.partyName}>welcome зима</span>
+      </h1>
       <LogoComponent isDarkProp={true} className={styles.logo}/>
       <Suspense fallback="loading">
         <Form purpose={'event'} isDarkProps={true} className={styles.form}>
           <div className={styles.description}>
             <h2 className={styles.subtitle}>В программе:</h2>
             <ul className={styles.list}>
-              <li className={styles.listItem}>Мастер-класс по актерскому мастерству</li>
-              <li className={styles.listItem}>Игры и конкурсы</li>
-              <li className={styles.listItem}>Челлендж TrashBOX</li>
+              <li className={styles.listItem}>Музыкально-танцевальные игры</li>
+              <li className={styles.listItem}>JUST DANCE - вечеринка</li>
               <li className={styles.listItem}>Зажигательная дискотека</li>
             </ul>
+            <div className={styles.text}>
+              <span>Мероприятие состоится 7 декабря в 16:00</span>
+              <span>Адрес: ул. Льва Толстого 8</span>
+
+            </div>
           </div>
         </Form>
       </Suspense>
